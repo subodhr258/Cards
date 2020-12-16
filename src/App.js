a4912d675546126d36bce1a84087d4947806bb78
Subodh Rajpopat <subodhr2001@gmail.com>
2020-12-16 10:05:59 +0530

    Made JSON file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
+import data from './robots.json';
 
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
         <div class="container">
         <h1>RoboFriends</h1>
         <SearchBox />
-        <CardList robots={robots}/>
+        <CardList robots={data.robots}/>
         </div>
     )
 }
