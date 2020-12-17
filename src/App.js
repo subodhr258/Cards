7b6ee004a7e7de5281a963051e5812d4eec42e40
Utkarsh Agnihotri <56087687+utkhagni13@users.noreply.github.com>
2020-12-17 10:25:20 +0530

    Implemented Search Box
    Tried implementing the Search Box but it is not working, errors were found in console.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import CardList from './Components/CardList';
-import SearchBox from './Components/SearchBox';
-import data from './robots.json';
+import React, { Component } from "react";
+import CardList from "./Components/CardList";
+import SearchBox from "./Components/SearchBox";
+import data from "./robots.json";
+import "./App.css";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: data.robots,
+      searchfields: "",
+    };
+  }
 
-const App = () => {
+  onSearchChange = (event) => {
+    this.setState({ searchfields: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfields.toLowerCase());
+    });
     return (
-        <div class="container">
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={data.robots} />
+      <div class="container">
+        <div className="tc">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
         </div>
-    )
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
